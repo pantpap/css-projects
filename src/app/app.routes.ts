4dbcdf5419610793a2d162fdda177b162595131c
Pantelis Papasavvas <pantpap@gmail.com>
2025-03-02 22:46:58 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: "full",
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    loadComponent:() => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
+  },
+  {
+    path: 'grid',
+    loadComponent:() => import('./grid/grid.component').then(c => c.GridComponent)
+  },
+  {
+    path: 'website',
+    loadComponent:() => import('./website/website.component').then(c => c.WebsiteComponent)
+  }
+];
