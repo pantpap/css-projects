05a1a962350f3199dc061fe787ef3a5ad13f9928
Pantelis Papasavvas <pantpap@gmail.com>
2025-04-26 01:37:15 +0300

    blog responsive and corrections

--- src/app/dopetrope/drope-blog/drope-blog.component.ts
+++ src/app/dopetrope/drope-blog/drope-blog.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faComment, faFileAlt} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: 'app-drope-blog',
-    imports: [],
+  imports: [
+    FaIconComponent
+  ],
     templateUrl: './drope-blog.component.html',
     styleUrl: './drope-blog.component.scss'
 })
 export class DropeBlogComponent {
-
+  protected readonly faFileAlt = faFileAlt;
+  protected readonly faComment = faComment;
 }
